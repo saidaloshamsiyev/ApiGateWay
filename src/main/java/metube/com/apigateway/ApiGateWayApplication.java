43985a6e69d4e4719cd8be98e3c20487a50c6231
said4ik <muxammadaminartikov@gmail.com>
2024-09-27 11:33:00 +0500

    Initial commit

---
+++ src/main/java/metube/com/apigateway/ApiGateWayApplication.java
@@ -0,0 +1,13 @@
+package metube.com.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateWayApplication.class, args);
+    }
+
+}
